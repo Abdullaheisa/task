bb318fc8b730989be5efa92f75d7558b4631fb38
abdullah eissa <109217917+Abdullaheisa@users.noreply.github.com>
2025-04-11 20:05:58 +0200

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/task/',  // تأكد من أن المسار هنا يشير إلى اسم المستودع الصحيح
+});
